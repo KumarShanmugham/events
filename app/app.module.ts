a79296c7706c95ba037966fd57700257fdc1a284
kumar.shanmugham <kumar.shanmugham@gmail.com>
2017-05-29 08:25:28 -0700

    included barrels

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventListResolver,
+    EventRouteActivator
+} from './events/index';
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventListResolver } from './events/events-list-resolver.service';
 import { AppRoutes } from './routes';
 
 @NgModule({
